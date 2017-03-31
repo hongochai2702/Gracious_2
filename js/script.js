7fa656e35caa31b07209158da4ea67ea9c61180f
Hai Ho <hongochai0401@gmail.com>
2017-03-31 22:44:29 +0700

    Upload Source lan 1

---
+++ js/script.js
@@ -0,0 +1,228 @@
+
+$(document).ready(function() {
+
+	"use strict";
+
+
+
+
+<!-- =============================================== -->
+<!-- ========== bootstrap scrollspy ========== -->
+<!-- =============================================== -->
+    
+    
+    $('body').scrollspy({
+    	target: '.navbar',
+    	offset: 160
+    });
+
+
+
+<!-- =============================================== -->
+<!-- ========== scrollTop.js ========== -->
+<!-- =============================================== -->
+    
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 2000);
+return false;
+});
+
+
+
+<!-- =============================================== -->
+<!-- ========== scrolldown.js ========== -->
+<!-- =============================================== -->
+
+$('.scrolldown a').bind('click', function () {
+    $('html , body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 160
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+<!-- =============================================== -->
+<!-- ========== navbardown.js ========== -->
+<!-- =============================================== -->
+
+$('.nav a').bind('click', function () {
+    $('html , body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+
+
+<!-- =============================================== -->
+<!-- ========== Banner rotator ========== -->
+<!-- =============================================== -->
+
+$('#slides').superslides({
+    animation: "fade",
+    play: 10000,
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: false,
+    scrollable: true
+  });    
+
+
+
+<!-- =============================================== -->
+<!-- ========== skillbar ========== -->
+<!-- =============================================== -->
+
+
+
+  var skillBarTopPos = jQuery('.skillbar').position().top;
+  jQuery(document).scroll(function(){
+    var scrolled_val = $(document).scrollTop().valueOf();
+    if(scrolled_val>skillBarTopPos-250) startAnimation();
+  });
+
+  function startAnimation(){
+    jQuery('.skillbar').each(function(){
+      jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+      },6000);
+    });
+  };
+
+
+
+
+
+<!-- =============================================== -->
+<!-- ========== fancy box ========== -->
+<!-- =============================================== -->
+
+
+
+
+	$(".youtube-media").on("click", function(e) {
+		var jWindow = $(window).width();
+		if (jWindow <= 410) {
+			return;
+		}
+		$.fancybox({
+			href: this.href,
+			padding: 4,
+			type: "iframe",
+			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+		});
+		return false;
+	});
+
+
+
+<!-- =============================================== -->
+<!-- ========== portfolio section ========== -->
+<!-- =============================================== -->
+
+
+
+$('#portfolio a').nivoLightbox({
+	effect: 'fadeScale'
+});
+
+
+
+<!-- =============================================== -->
+<!-- ========== owlcarousel team ========== -->
+<!-- =============================================== -->
+
+
+
+/*$(document).ready(function() {*/
+$("#owl-carousel-team").owlCarousel({
+
+/*autoPlay: 3000, //Set AutoPlay to 3 seconds*/
+items : 4,
+itemsDesktop : [1199,3],
+itemsDesktopSmall : [979,3],
+
+pagination : true,
+paginationNumbers: false,
+
+
+responsive: true,
+responsiveRefreshRate : 200,
+responsiveBaseWidth: window
+
+});
+
+
+ new WOW().init();
+
+}); 
+
+
+
+
+
+
+<!-- =============================================== -->
+<!-- ========== Navbar ========== -->
+<!-- =============================================== -->
+
+
+	jQuery(window).scroll(function () {
+	  var top = jQuery(document).scrollTop();
+		var batas = jQuery(window).height();
+	  //alert(batas);
+	  
+	  if (top > batas) {
+		jQuery('.main-menu').addClass('tiny');
+		jQuery('.main-menu').css('opacity','1');
+	  } else {
+	   jQuery('.main-menu').removeClass('tiny'); 
+	  }
+	});
+
+
+
+<!-- =============================================== -->
+<!-- ========== Map ========== -->
+<!-- =============================================== -->
+
+
+$(document).ready(function() {
+
+
+ google.maps.event.addDomListener(window, 'load', init);
+        
+            function init() {
+                // Basic options for a simple Google Map
+                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+                var mapOptions = {
+                    // How zoomed in you want the map to start at (always required)
+                    zoom: 11,
+
+                    // The latitude and longitude to center the map (always required)
+                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+
+                    // How you would like to style the map. 
+                    // This is where you would paste any style found on Snazzy Maps.
+                    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ec46c0"},{"visibility":"on"}]}]
+                };
+
+                // Get the HTML DOM element that will contain your map 
+                // We are using a div with id="map" seen below in the <body>
+                var mapElement = document.getElementById('map');
+
+                // Create the Google Map using our element and options defined above
+                var map = new google.maps.Map(mapElement, mapOptions);
+
+                // Let's also add a marker while we're at it
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(40.6700, -73.9400),
+                    map: map,
+                    title: 'Snazzy!'
+                });
+            }
+
+
+});	
